d9a52962e028a224b9a6bff5ac1dd5a0de368e33
karimmohamed995 <kimo.mohamed94@gmail.com>
2025-07-11 15:07:22 +0300

    evently

---
+++ lib/ui/utilities/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  static const String splash = "assets/images/splash.png";
+  static const String logoVertical = "assets/images/logo1.png";
+  static const String logoHorizontal = "assets/images/logo2.png";
+  static const String mailIcon = "assets/images/email_icon.png";
+  static const String passIcon = "assets/images/pass_icon.png";
+}
